f116d29f14a41fb661e69ae117a2fda803d55a3c
wiselife51 <wiselife51@gmail.com>
2026-02-03 01:55:02 +0000

    feat: implement Swiper for carousels and fix build errors

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,7 +5,7 @@
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_22
+    pkgs.nodejs_20
   ];
   # Sets environment variables in the workspace
   env = {};
--- src/components/section/HowItWorks/HowItWorks.tsx
+++ src/components/section/HowItWorks/HowItWorks.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCoverflow, Pagination, Autoplay, Navigation } from 'swiper/modules';
 
